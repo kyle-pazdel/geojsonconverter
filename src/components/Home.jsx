4fe25cf10c85a3c0e83fa754f8f51bdb037290c7
Kyle Pazdel <kyle.kayvaun@gmail.com>
2023-03-07 09:17:42 -0800

    Adds env for api key

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,25 @@
+import Map from "react-map-gl";
+import Dropzone from "react-dropzone";
+import { useEffect } from "react";
 
+export default function Home() {
+  const key = import.meta.env.VITE_ACCESS_KEY;
 
-export default function Home(){
-
-  render (
+  return (
     <>
-      <></>
+      <div>
+        <p>{key}</p>
+      </div>
+      <Map
+        initialViewState={{
+          longitude: -122.4,
+          latitude: 37.8,
+          zoom: 14,
+        }}
+        style={{ width: 600, height: 400 }}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        mapboxAccessToken={key}
+      />
     </>
-  )
+  );
 }
