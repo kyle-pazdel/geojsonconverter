dba700ec2db008d6548ff7374c2757f9bca61431
Kyle Pazdel <kyle.kayvaun@gmail.com>
2023-03-07 11:21:56 -0800

    Adds dropzone to Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,67 @@
 import Map from "react-map-gl";
-import Dropzone from "react-dropzone";
-import { useEffect } from "react";
+import Dropzone, { useDropzone } from "react-dropzone";
+import { useEffect, useCallback, useMemo } from "react";
+import gpxParser from "gpxparser";
 
 export default function Home() {
   const key = import.meta.env.VITE_ACCESS_KEY;
 
+  const onDrop = useCallback((acceptedFiles) => {
+    acceptedFiles.forEach((file) => {
+      const reader = new FileReader();
+      console.log(file);
+      file.reader;
+      reader.onabort = () => console.log("file reading was aborted");
+      reader.onerror = () => console.log("file reading has failed");
+      reader.onload = () => {
+        const binaryStr = reader.result;
+        let gpx = new gpxParser();
+        parsed = gpx.parse(binaryStr);
+        console.log(parsed);
+      };
+    });
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive, isDragAccept, isDragReject } = useDropzone({ onDrop });
+
+  // react-dropzone styling
+  const baseStyle = {
+    flex: 1,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    padding: "20px",
+    borderWidth: 2,
+    borderRadius: 2,
+    borderColor: "#eeeeee",
+    borderStyle: "dashed",
+    backgroundColor: "#fafafa",
+    color: "#bdbdbd",
+    outline: "none",
+    transition: "border .24s ease-in-out",
+  };
+  const activeStyle = {
+    borderColor: "#2196f3",
+  };
+  const acceptStyle = {
+    borderColor: "#00e676",
+  };
+  const rejectStyle = {
+    borderColor: "#ff1744",
+  };
+  const style = useMemo(
+    () => ({
+      ...baseStyle,
+      ...(isDragActive ? activeStyle : {}),
+      ...(isDragAccept ? acceptStyle : {}),
+      ...(isDragReject ? rejectStyle : {}),
+    }),
+    [isDragActive, isDragReject, isDragAccept]
+  );
+
   return (
     <>
-      <div>
-        <p>{key}</p>
-      </div>
-      <Map
+      {/* <Map
         initialViewState={{
           longitude: -122.4,
           latitude: 37.8,
@@ -19,7 +70,15 @@ export default function Home() {
         style={{ width: 600, height: 400 }}
         mapStyle="mapbox://styles/mapbox/streets-v9"
         mapboxAccessToken={key}
-      />
+      /> */}
+      <div {...getRootProps({ style })}>
+        <input {...getInputProps()} />
+        {isDragActive ? (
+          <p>Drag 'n' drop GPX files here</p>
+        ) : (
+          <p>Drag 'n' drop some files here, or click to select files</p>
+        )}
+      </div>
     </>
   );
 }
