34f2688fd4d178561b158bf95774ac8eff9ae1bf
Kyle Pazdel <kyle.kayvaun@gmail.com>
2023-03-08 10:07:12 -0800

    Adds logo and favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import GpxToGeoJSON from "./components/GpxToGeoJSON";
 import KmlToGeoJSON from "./components/KmlToGeoJSON";
+import image from "./assets/geo-text.svg";
 
 function App() {
   return (
     <>
+      <img src={image}></img>
       <GpxToGeoJSON />
       <KmlToGeoJSON />
     </>
